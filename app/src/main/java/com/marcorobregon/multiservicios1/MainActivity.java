44b4740ee1a1fa5216f974d8af055735c7a6b8d2
cuateverde <ikermarcor5@gmail.com>
2022-11-21 19:40:31 -0700

    Primer commit se añade: -login

---
+++ app/src/main/java/com/marcorobregon/multiservicios1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.marcorobregon.multiservicios1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
